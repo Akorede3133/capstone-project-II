969fa7311d4f0e84de7318a1b46672d143e713a0
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-08-03 03:37:01 -0700

    Display number of likes

--- src/modules/fetch_api.js
+++ src/modules/fetch_api.js
@@ -1,6 +1,8 @@
 class FetchData {
   showsUrl = 'https://api.tvmaze.com/shows';
 
+  postLikeUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
+
   fetchShows = async () => {
     try {
       const response = await fetch(this.showsUrl);
@@ -14,6 +16,34 @@ class FetchData {
   fetchSingleShows = async (id) => {
     try {
       const response = await fetch(`${this.showsUrl}/${id}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  postLike = async (id, showId) => {
+    try {
+      const response = await fetch(`${this.postLikeUrl}/${showId}/likes/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          item_id: id,
+        }),
+      });
+      const data = await response.text();
+      return data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  getLikes = async (showId) => {
+    try {
+      const response = await fetch(`${this.postLikeUrl}/${showId}/likes`);
       const data = await response.json();
       return data;
     } catch (error) {
