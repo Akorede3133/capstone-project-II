847fee6161d68884f712d69cbbe43fecedde743f
akorede <emiolasaheed1@gmail.com>
2023-08-03 09:39:08 +0100

    Add functionlity to display comment popup

--- src/modules/show_class.js
+++ src/modules/show_class.js
@@ -1,6 +1,13 @@
+import fecthData from './fetch_api.js';
+
 class Show {
   constructor() {
     this.showsContainer = document.querySelector('.shows--container');
+    this.popUpContainer = document.querySelector('.single--show--detail');
+    this.popUpSection = document.querySelector('.pop--up--section');
+    this.closePopUpBtn = document.querySelector('.fa-close');
+    this.windowHeight = window.scrollY;
+
     // this.showId = '7eeZSxoE9qlr2meDvNAi';
     // this.showId = 'HA6T9JcmdjaALtObT86k';
     this.showId = 'Zrj6oLFHff3PIwzqVfDg';
@@ -10,18 +17,8 @@ class Show {
     // Zrj6oLFHff3PIwzqVfDg
   }
 
-  fetchShows = async (url) => {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      return error;
-    }
-  }
-
   displayShows = async () => {
-    const shows = await this.fetchShows('https://api.tvmaze.com/shows');
+    const shows = await fecthData.fetchShows();
     const elements = shows.slice(0, 30).map((show) => {
       const {
         image: { medium }, name, id,
@@ -47,8 +44,64 @@ class Show {
       return showElement;
     }).join('');
     this.showsContainer.insertAdjacentHTML('beforeend', elements);
+    this.displayPopUp();
+    this.hidePopUp();
+  }
+
+  displayPopUp = () => {
+    const commentBtns = this.showsContainer.querySelectorAll('.comments--btn');
+    commentBtns.forEach((btn) => {
+      btn.addEventListener('click', async (e) => {
+        const { id } = e.target;
+        try {
+          const shows = await fecthData.fetchSingleShows(id);
+          window.scrollTo(0, 0);
+          this.windowHeight = window.scrollY;
+          this.CreatePopUpInterface(shows);
+        } catch (error) {
+          return error;
+        }
+        return id;
+      });
+    });
+  }
+
+  CreatePopUpInterface = async (data) => {
+    const {
+      language, name, genres, rating: { average }, weight, premiered, image: { original }, summary,
+    } = data;
+    this.popUpContainer.innerHTML = '';
+    const element = ` <div class="pop--up--img">
+    <img src=${original} alt="">
+  </div>
+  <div class="pop--up--desc">
+    <h3 class="show--name">${name}</h3>
+    <p>${summary}</p>
+    <ul class="pop--up--info">
+      <li class="show--title">Name: <span>${name}</span></li>
+      <li class="">Language: <span>${language}</span></li>
+      <li class="show--rating">Rating: <span>${average}</span></li>
+      <li class="show--weight">Weight: <span>${weight}</span></li>
+      <li class="show--genre">Generes: <span>${genres[0]}</span></li>
+      <li class="show--premiered">premiered: <span>${premiered}</span></li>
+    </ul>
+  </div>
+  <div class="comments--container">
+    <h4>Comments (<span class="comment--count"></span>)</h4>
+    <ul class="comments--list">
+    </ul>
+  </div>`;
+    this.popUpContainer.insertAdjacentHTML('beforeend', element);
+    // this.popUpSection.style.display = 'block';
+    this.popUpSection.classList.remove('hide--pop--up');
+  }
+
+  hidePopUp = () => {
+    this.closePopUpBtn.addEventListener('click', () => {
+      window.scrollTo(0, this.windowHeight);
+      this.popUpSection.classList.add('hide--pop--up');
+    });
   }
 }
 const show = new Show();
-
 export default show;
