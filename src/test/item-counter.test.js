ba719072122ee55c949d7d4af10a6bcb05fb8233
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-08-04 01:31:26 -0700

    Test for count shows function

---
+++ src/test/item-counter.test.js
@@ -0,0 +1,24 @@
+import counter from '../modules/counter.js';
+describe('Shows counter test', () => {
+  const shows = [
+    {
+      name: 'The 100',
+      image: 'the100.png',
+      id: '1',
+    },
+    {
+      name: 'The flash',
+      image: 'theflash.png',
+      id: '2',
+    },
+    {
+      name: 'Into the badlands',
+      image: 'intothebadlands.png',
+      id: '3',
+    },
+  ];
+  test('count number of shows', () => {
+    const showCount = counter.countShows(shows);
+    expect(showCount).toBe(3);
+  });
+});
